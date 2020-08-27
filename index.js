464e192a5525db64e83a0152639be0e815884754
Juan Calle <juanfcalle@gmail.com>
2020-08-27 10:47:27 -0500

    Drawing animations - OnTick and OnComplete

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ class Timer {
     this.pauseButton = pauseButton;
     if (callbacks) {
       this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
 
     this.startButton.addEventListener('click', this.start);
@@ -26,9 +28,15 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     }
     else {
       this.timeRemaining -= 1;
+      if (this.onTick) {
+        this.onTick();
+      }
     }
   }
 
@@ -48,5 +56,11 @@ const pauseButton = document.querySelector('#pause');
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart(){
     console.log("timer started");
+  },
+  onTick(){
+    console.log("timer just ticked down");
+  },
+  onComplete(){
+    console.log("timer is complete");
   }
 });
